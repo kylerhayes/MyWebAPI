9809b5cf720a61bfd1c0ca5aac48fb7eeccc8cc1
KylerH4192 <hayes2k4@mail.uc.edu>
2025-07-12 14:18:15 -0400

    Added NameController to return JSON with my name

---
+++ MyWebAPI/Controllers/NameController.cs
@@ -0,0 +1,16 @@
+﻿//kyler hayes
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NameController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { name = "Kyler Hayes" });
+        }
+    }
+}
